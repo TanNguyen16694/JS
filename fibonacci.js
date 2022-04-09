ec472daef3ef3650c6ea33e05eceeab2e88183e6
Tân Nguyễn <tannguyen16694@gmail.com>
2022-04-09 21:35:46 +0700

    change file fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,16 +1,17 @@
 // program to generate fibonacci series up to n terms
 
 // take input from the user
-const number = parseInt(prompt("Enter the number of terms: "));
-let n1 = 0,
-  n2 = 1,
-  nextTerm;
+// const number = parseInt(prompt("Enter the number of terms: "));
+// let n1 = 0,
+//   n2 = 1,
+//   nextTerm;
 
-console.log("Fibonacci Series:");
+var age = 17.5;
 
-for (let i = 1; i <= number; i++) {
-  console.log(n1);
-  nextTerm = n1 + n2;
-  n1 = n2;
-  n2 = nextTerm;
+var canByAlcohol = 18;
+
+if (age >= canByAlcohol) {
+  console.log("Được Phép Mua Rượu");
+} else {
+  console.log("Không Được Phép Mua Rượu");
 }
